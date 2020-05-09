543208df77d3ef9e4f102dcf1e46b8e11fb0a47a
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-09 16:43:47 +1000

    Update admin.py

--- airline0/flights/admin.py
+++ airline0/flights/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra = 1
+
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights")
+
+admin.site.register(Airport)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
