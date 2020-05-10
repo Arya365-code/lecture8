f0e1fec820d6161679b5a2c766a86d0ae1d60089
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-10 15:07:37 +1000

    Update tests.py

--- airline2/flights/tests.py
+++ airline2/flights/tests.py
@@ -47,3 +47,33 @@ class ModelsTestCase(TestCase)):
         response = c.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["flights"].count(), 2)
+    
+    def test_valid_flight_page(self):
+        a1 = Airport.objects.get(code="AAA")
+        f = Flight.objects.get(origin=a1, destination=a1)
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_invalid_flight_page(self):
+        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
+        c = Client()
+        response = c.get(f"/{max_id + 1}")
+        self.assertEqual(repsonse.status_code, 404)
+    
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+        f.passengers.add(p)
+        c = Client()
+        response = c.get(f"/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+    
+    def test_flight_page_non_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+        c = Client()
+        response = c.get(f"/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["non-passengers"].count(), 1)
